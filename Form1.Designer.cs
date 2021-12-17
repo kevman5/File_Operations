2c53733f1e3e04d65c77b8c813312c04fdb53228
KevCodes <41352514+kevman5@users.noreply.github.com>
2021-12-17 17:41:37 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace file_operationschpt12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tb_1 = new System.Windows.Forms.TextBox();
+            this.tb_fname = new System.Windows.Forms.TextBox();
+            this.tb_add = new System.Windows.Forms.TextBox();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.tb_Lname = new System.Windows.Forms.TextBox();
+            this.tb_ins = new System.Windows.Forms.TextBox();
+            this.tb_phone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // tb_1
+            // 
+            this.tb_1.Location = new System.Drawing.Point(59, 75);
+            this.tb_1.Name = "tb_1";
+            this.tb_1.Size = new System.Drawing.Size(262, 20);
+            this.tb_1.TabIndex = 1;
+            // 
+            // tb_fname
+            // 
+            this.tb_fname.Location = new System.Drawing.Point(96, 176);
+            this.tb_fname.Name = "tb_fname";
+            this.tb_fname.Size = new System.Drawing.Size(133, 20);
+            this.tb_fname.TabIndex = 2;
+            // 
+            // tb_add
+            // 
+            this.tb_add.Location = new System.Drawing.Point(316, 176);
+            this.tb_add.Name = "tb_add";
+            this.tb_add.Size = new System.Drawing.Size(133, 20);
+            this.tb_add.TabIndex = 3;
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(316, 229);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(133, 20);
+            this.tb_email.TabIndex = 4;
+            // 
+            // tb_Lname
+            // 
+            this.tb_Lname.Location = new System.Drawing.Point(96, 229);
+            this.tb_Lname.Name = "tb_Lname";
+            this.tb_Lname.Size = new System.Drawing.Size(133, 20);
+            this.tb_Lname.TabIndex = 5;
+            // 
+            // tb_ins
+            // 
+            this.tb_ins.Location = new System.Drawing.Point(316, 281);
+            this.tb_ins.Name = "tb_ins";
+            this.tb_ins.Size = new System.Drawing.Size(133, 20);
+            this.tb_ins.TabIndex = 6;
+            // 
+            // tb_phone
+            // 
+            this.tb_phone.Location = new System.Drawing.Point(96, 281);
+            this.tb_phone.Name = "tb_phone";
+            this.tb_phone.Size = new System.Drawing.Size(133, 20);
+            this.tb_phone.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Phone No.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Address";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(249, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Insurance";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_phone);
+            this.Controls.Add(this.tb_ins);
+            this.Controls.Add(this.tb_Lname);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.tb_add);
+            this.Controls.Add(this.tb_fname);
+            this.Controls.Add(this.tb_1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.TextBox tb_1;
+        private System.Windows.Forms.TextBox tb_fname;
+        private System.Windows.Forms.TextBox tb_add;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.TextBox tb_Lname;
+        private System.Windows.Forms.TextBox tb_ins;
+        private System.Windows.Forms.TextBox tb_phone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace file_operationschpt12
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String infofile;
+            openFileDialog1.ShowDialog();
+            infofile = openFileDialog1.FileName;
+            tb_1.Text = infofile;
+            
+
+            using (StreamReader fileReader = new StreamReader(infofile))
+            {
+                string inputrecord = fileReader.ReadLine();
+                string[] inputfields;
+               
+                if (inputrecord != null)
+                {
+                    inputfields = inputrecord.Split(',');
+                    tb_fname.Text = inputfields[0];
+                    tb_Lname.Text = inputfields[1];
+                    tb_phone.Text = inputfields[2];
+                    tb_add.Text = inputfields[3];
+                    tb_email.Text = inputfields[4];
+                    tb_ins.Text = inputfields[5];
+                    
+
+                }
+                fileReader.Close();
+            }
+
+        }
+
+    
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            String infofile = "C:\\temp\\infofile.txt";
+            infofile = saveFileDialog1.FileName;
+            tb_1.Text = infofile;
+            TextWriter tw = new StreamWriter(infofile);
+            String s, s1;
+            s = tb_fname.Text.ToString() + "," + tb_Lname.Text.ToString() + "," + tb_phone.Text.ToString() + "," + tb_add.Text.ToString() + "," + tb_email.Text.ToString() + "," + tb_ins.Text.ToString();
+            
+            tw.WriteLine(s);
+            
+            tw.Close();
+
+
+
+        }
+    }
+}
